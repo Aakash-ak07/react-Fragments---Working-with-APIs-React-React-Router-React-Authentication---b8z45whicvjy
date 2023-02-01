24b9d3bfa1cc694586cf008e4ea7e746f3d42480
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-02-01 17:33:29 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,14 @@
 import React, { useState, useEffect, Fragment } from "react";
 import '../styles/App.css';
-
 import List from "./List";
 
 const App = () => {
- 
   const [value, setValue] = useState(0);
   const [list, setList] = useState([]);
 
-
   const onButtonClick = () => {
-    setList([])
-    for (let i = 1; i < Number(value)+1; i++) {
-      
-      setList((prev) => [...prev, i]);
-    }
-   
+   setList(Array.from({ length: `${value}` }, (v, i) => i))
   };
- 
 
   const onInputChange = (e) => {
     setValue(e.target.value);
@@ -25,11 +16,14 @@ const App = () => {
 
   return (
     <div id="main">
-
       <input id="input" onChange={onInputChange} />
       <button id="button" onClick={onButtonClick}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <ul id="list">
+        {
+          list.map((item,i) => {
+            return <List listx={i+1}/>
+          })
+        }
       </ul>
     </div>
   );
